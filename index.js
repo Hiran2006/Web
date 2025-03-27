c73808a62a4182aa1e61397825a1ec266d477e88
Hiran <hirans2006@gmail.com>
2025-03-27 12:33:50 +0530

    version mention

--- index.js
+++ index.js
@@ -12,3 +12,14 @@ function loadRepositories() {
       });
     });
 }
+if (
+  navigator.userAgent.match(/Android/i) ||
+  navigator.userAgent.match(/webOS/i) ||
+  navigator.userAgent.match(/iPhone/i) ||
+  navigator.userAgent.match(/iPad/i) ||
+  navigator.userAgent.match(/iPod/i) ||
+  navigator.userAgent.match(/BlackBerry/i) ||
+  navigator.userAgent.match(/Windows Phone/i)
+) {
+  alert("This website version for desktop only.");
+}
