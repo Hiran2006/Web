e903db5bd20dea4c221b009e745e54ee2e851532
Hiran <hirans2006@gmail.com>
2025-04-19 18:37:19 +0530

    version update

--- public/index.js
+++ public/index.js
@@ -1,1 +1,11 @@
-
+if (
+  /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+    navigator.userAgent
+  )
+);
+else {
+  alert(
+    "Only avaliable on mobile devices. Please use a mobile device to access this page."
+  );
+  history.back();
+}
